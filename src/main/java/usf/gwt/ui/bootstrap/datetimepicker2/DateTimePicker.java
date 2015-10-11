d34a9e24d50410f413cc601886489e7b368057a4
uSf <usf.alami@gmail.com>
2015-10-11 16:18:48 +0200

    datetimepicker2 functions

--- src/main/java/usf/gwt/ui/bootstrap/datetimepicker2/DateTimePicker.java
+++ src/main/java/usf/gwt/ui/bootstrap/datetimepicker2/DateTimePicker.java
@@ -1,91 +1,78 @@
 package usf.gwt.ui.bootstrap.datetimepicker2;
 
+import java.util.Date;
+
 import usf.gwt.ui.bootstrap.client.TextBox;
 import usf.gwt.ui.bootstrap.datetimepicker.client.JavaScriptOption;
 
 import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.core.client.JsDate;
+import com.google.gwt.dom.client.Element;
 
 public class DateTimePicker extends TextBox {
 	
     protected static final String DATE_TIME_PICKER = "DateTimePicker";
 
 	protected JavaScriptObject picker;
-	protected JavaScriptOption option;
 
 	public DateTimePicker() {
-		this.option = defaultDateTimePickerOptions();
+		this(defaultDateTimePickerOptions());
 	}
 	public DateTimePicker(JavaScriptOption option) {
-		this.option = option;
+		picker = render(getBaseElement(), option);
 	}
 
-	@Override
-	protected void onLoad() {
-		if(picker == null)
-			render();
-	}
 	/**
 	 * @see http://momentjs.com/docs/#/displaying/format/
 	 * @param format
 	 */
 	public final native JavaScriptObject setFormat(String format) /*-{
-		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::option["format"] = format;
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		picker && picker.format(format);
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.format(format);
 	}-*/;
-	public final native void getFormat() /*-{
-		return picker == null ? null : picker.format();
+	public final native String getFormat() /*-{
+		return this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.format();
 	}-*/;
 
+	public final native void setDate(Date date) /*-{
+		var c = @usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::convertDate(Ljava/util/Date;);
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.date(date ? new $wnd.Date(c(date)) : null);
+	}-*/;
+	public final native Date getDate() /*-{
+		var d = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.date();
+		return d ? @usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::convertDate(D)(d) : null;
+	}-*/;
 
-	public final native void setJsDate(JsDate date) /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		return picker == null ? null : picker.date(date);
+	public final native void setDefaultDate(Date date) /*-{
+		var c = @usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::convertDate(Ljava/util/Date;);
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.defaultDate(date ? new $wnd.Date(c(date)) : null);
 	}-*/;
-	public final native JsDate getJsDate() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		return picker == null ? null : picker.date();
-	}-*/;
-
-	public final native void setDefaultJsDate(JsDate date) /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::option["defaultDate"] = date;
-		return picker && picker.defaultDate(date);
-	}-*/;
-	public final native JsDate getDefaultJsDate() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		return picker == null ? null : picker.defaultDate();
+	public final native Date getDefaultDate() /*-{
+		var d = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.defaultDate();
+		return d ? @usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::convertDate(D)(d) : null;
 	}-*/;
 	
-	public final native void setMinJsDate(JsDate date) /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::option["minDate"] = date;
-		return picker == null ? null : picker.minDate(date);
+	public final native void setMinDate(Date date) /*-{
+		var c = @usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::convertDate(Ljava/util/Date;);
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.minDate(date ? new $wnd.Date(c(date)) : null);
 	}-*/;
-	public final native JsDate getMinJsDate() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		return picker == null ? null : picker.minDate();
+	public final native Date getMinDate() /*-{
+		var d = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.minDate();
+		return d ? @usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::convertDate(D)(d) : null;
 	}-*/;
-	public final native void setMaxJsDate(JsDate date) /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::option["maxDate"] = date;
-		return picker == null ? null : picker.maxDate(date);
+	public final native void setMaxDate(Date date) /*-{
+		var c = @usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::convertDate(Ljava/util/Date;);
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.maxDate(date ? new $wnd.Date(c(date)) : null);
 	}-*/;
-	public final native JsDate getMaxJsDate() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		return picker == null ? null : picker.maxDate();
+	public final native Date getMaxDate() /*-{
+		var d = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.maxDate();
+		return d ? @usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::convertDate(D)(d) : null;
 	}-*/;
 	
 
 	public final native void setOption(JavaScriptOption option) /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::option = option;
-		return picker == null ? null : picker.option(option);
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.option(option);
 	}-*/;
-	public final native JsDate getOption() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		return picker == null ? null : picker.option();
+	public final native JavaScriptObject getOption() /*-{
+		return this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.option();
 	}-*/;
 	
 	//action functions
@@ -93,46 +80,46 @@ public class DateTimePicker extends TextBox {
 	 * Destroys the widget and removes all attached event listeners
 	 */
 	public final native void destroy() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		picker && picker.destroy();
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.estroy();
 	}-*/;
 	/**
 	 * Shows or hides the widget
 	 */
 	public final native void show() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		picker && picker.show();
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.show();
 	}-*/;
 	/**
 	 * Hides the widget
 	 */
 	public final native void hide() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		picker && picker.hide();
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.hide();
 	}-*/;
 	/**
 	 * Shows or hides the widget
 	 */
 	public final native void toggole() /*-{
-		var picker = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker;
-		picker && picker.toggole();
+		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker.toggole();
 	}-*/;
+	
+	protected static double convertDate(Date date) {
+		return date.getTime();
+	}
+	protected static Date convertDate(double date) {
+		return new Date ((long)date);
+	}
 	
 	/**
 	 * creates datetimepicker javascript object
 	 * 
 	 */
-	protected final native void render() /*-{
-		var e = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::getElement()();
-		var o = this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::option;
-		this.@usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker::picker =
-			$wnd.$(e).datetimepicker(o).data("DateTimePicker");
+	protected final static native JavaScriptObject render(Element e, JavaScriptObject o) /*-{
+		return $wnd.deb = $wnd.$(e).datetimepicker(o).data("DateTimePicker");
 	}-*/;
 
-	public static final JavaScriptOption defaultDateTimePickerOptions(){
+	public final static JavaScriptOption defaultDateTimePickerOptions(){
 		return JavaScriptOption.createOption()
-		.set("locale", "fr")
-		.set("format", "L");
+		.set("locale", "fr");
+//		.set("format", "L");
 	}
 
 }
--- src/main/java/usf/gwt/ui/demo/sample/TestSampes.java
+++ src/main/java/usf/gwt/ui/demo/sample/TestSampes.java
@@ -1,13 +1,20 @@
 package usf.gwt.ui.demo.sample;
 
+import java.util.Date;
+
 import usf.gwt.ui.bootstrap.client.Anchor;
+import usf.gwt.ui.bootstrap.client.Button;
 import usf.gwt.ui.bootstrap.client.Group;
 import usf.gwt.ui.bootstrap.client.Panel;
 import usf.gwt.ui.bootstrap.datetimepicker2.DateTimePicker;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -18,6 +25,9 @@ public class TestSampes extends Composite {
 
 	@UiField Anchor anchor;
 	@UiField Group group;
+	
+
+	DateTimePicker pick;
 		
 	public TestSampes() {
 		Panel p = new Panel();
@@ -25,9 +35,27 @@ public class TestSampes extends Composite {
 		
 //		Window.alert("uSf");
 		
-		DateTimePicker pick = new DateTimePicker();
-		pick.show();
-		p.add(pick);
+		p.add(pick = new DateTimePicker());
+
+		pick.setDate(DateTimeFormat.getFormat("dd/mm/yyyy").parse("21/10/1987"));
+		
+//		pick.setFormat("L");
+
+		Window.alert(pick.getDate() + "");
+
+		usf.gwt.ui.bootstrap.client.Button b = new Button("Change date");
+		b.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent arg0) {
+				pick.setDate(null);
+
+				Window.alert(pick.getDate() + "");
+			}
+		});
+		
+		p.add(b);
+		
+//		Window.alert(pick.getJsDate() + "");
 		
 	}
 	
