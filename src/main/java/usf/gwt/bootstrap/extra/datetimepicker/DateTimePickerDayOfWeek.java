9b7b5dacd287ea25f4e356689bb327a809d77913
uSf <usf.alami@gmail.com>
2015-10-14 21:26:35 +0200

    datetimepickre optim

--- src/main/java/usf/gwt/bootstrap/extra/datetimepicker/DateTimePickerDayOfWeek.java
+++ src/main/java/usf/gwt/bootstrap/extra/datetimepicker/DateTimePickerDayOfWeek.java
@@ -2,16 +2,13 @@ package usf.gwt.bootstrap.extra.datetimepicker;
 
 public enum DateTimePickerDayOfWeek {
 
-    SUNDAY(0), MONDAY(1), TUESDAY(2), WEDNESDAY(3), THURSDAY(4), FRIDAY(5), SATURDAY(6);
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
 
-    private int value;
-
-    DateTimePickerDayOfWeek(int value) {
-        this.value = value;
+    DateTimePickerDayOfWeek() {
     }
 
     public int getValue() {
-        return value;
+        return this.ordinal();
     }
     
     public static DateTimePickerDayOfWeek dayOf(int value){
