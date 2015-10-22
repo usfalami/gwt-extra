44f75cd4ad6b2067de1ce01ad179d7c08f8aecd6
YAH <youssef-externe.alami@erdf.fr>
2015-10-22 09:26:48 +0200

    DTP 4.17

--- src/main/java/com/gwt/extra/client/ExtraGwt.java
+++ src/main/java/com/gwt/extra/client/ExtraGwt.java
@@ -1,5 +1,8 @@
 package com.gwt.extra.client;
 
+import usf.gwt.bootstrap.extra.datetimepicker.DateTimePicker;
+import usf.gwt.bootstrap.ui.js.JavaScriptOption;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.gwt.extra.client.demo.Menu;
@@ -11,6 +14,14 @@ public class ExtraGwt implements EntryPoint {
 	
 	@Override
 	public void onModuleLoad() {
+		
+		JavaScriptOption icons = JavaScriptOption.createOption()
+				.set("time", "fa fa-clock-o").set("date", "fa fa-calendar").set("up", "fa fa-chevron-up")
+				.set("down", "fa fa-chevron-down").set("previous", "fa fa-chevron-left").set("next", "fa fa-chevron-right")
+				.set("today", "fa fa-crosshairs").set("clear", "fa fa-trash").set("close", "fa fa-remove");
+		
+		DateTimePicker.getDefaultOption().set("icons", icons);
+		
 		RootPanel.get("sidebar").add(new Menu());
 	}
 	
